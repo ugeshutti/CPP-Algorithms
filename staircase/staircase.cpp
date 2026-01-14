5f8cb1ff3a6070711240821e0032f18902f072c7
Ugesh Utti <33598652+ugeshutti@users.noreply.github.com>
2026-01-14 20:01:00 +0530

    Implement staircase pattern printing program
    This program prompts the user for a size 'n' and prints a staircase pattern of that size using '#' characters.

--- staircase/staircase.cpp
+++ staircase/staircase.cpp
@@ -1,1 +1,30 @@
 
+//Write a program that prints a staircase of size n
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i,k,j,p,q;
+    cout<<"Enter value of n: ";
+    cin>>n;
+    k=n-1;
+    for(i=0;i<n;i++)
+    {
+       for(j=0;j<n;j++)
+    {
+           if(j==k)
+           {
+               for(p=j;p<n;p++)
+               {
+                cout<<"#";
+               }
+               cout<<"\n";
+               break;
+           }
+           cout<<" ";
+    }
+      k--;   
+    }
+   return 0;
+}
