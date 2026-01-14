1350df83d989160ea4717ac72f86d4acc229cc46
Ugesh Utti <33598652+ugeshutti@users.noreply.github.com>
2026-01-14 20:14:59 +0530

    Refactor Min_Max_Sum.cpp for clarity and performance
    Refactored code to improve readability and maintainability. Added comments for clarity and corrected syntax for input/output operations.

--- Min_Max_Sum/Min_Max_Sum.cpp
+++ Min_Max_Sum/Min_Max_Sum.cpp
@@ -1,1 +1,45 @@
-Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+//Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+#include<iostream>
+#include <memory>
+
+using namespace std;
+
+int main() {
+    int i,j;
+    long int sum[10]={0},temp;
+    int *arr = new int[5];
+    cout<<"Enter 5 values of an array : ";
+    for(int arr_i = 0; arr_i < 5; arr_i++)
+    {
+        cin>>arr[arr_i];
+    }
+
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(i!=j)
+            {
+                sum[i]=sum[i]+arr[j];
+            }
+        }
+    
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=i+1;j<=4;j++)
+        {
+            if(sum[i]>sum[j])
+            {
+                temp=sum[j];
+                sum[j]=sum[i];
+                sum[i]=temp;
+            }
+        }  
+    }
+    cout<<sum[0]<<" "<<sum[4];
+
+    delete[] arr;
+    return 0;
+}
